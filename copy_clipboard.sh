557683485d6b1813d82b89c31098b71143b5e152
Ahsen Uppal <auppal.noreply.github.com>
2020-04-18 16:21:18 -0400

    Space substitution compatibility for bash.

--- copy_clipboard.sh
+++ copy_clipboard.sh
@@ -2,7 +2,7 @@
 #
 copy_to_clipboard() {
     if  [ $DISPLAY ] && which xclip >& /dev/null; then
-	printf "%s" $CUTBUFFER | xclip -i
+	printf "%s" "$CUTBUFFER" | xclip -i
     else
 	copy_osc52
     fi
@@ -11,11 +11,11 @@ copy_to_clipboard() {
 copy_osc52() {
     if  [[ $TERM = 'screen' || $TERM = 'screen-256color' ]]; then
 	if  [ $TMUX ]; then
-	    printf "\ePtmux;\e\e]52;c;$(printf '%s' $CUTBUFFER | base64 --wrap=0)\a\e\\"
+	    printf "\ePtmux;\e\e]52;c;$(printf '%s' "$CUTBUFFER" | base64 --wrap=0)\a\e\\"
 	else
-	    printf "\eP\e]52;c;$(printf '%s' $CUTBUFFER | base64 --wrap=0)\a\e\\"
+	    printf "\eP\e]52;c;$(printf '%s' "$CUTBUFFER" | base64 --wrap=0)\a\e\\"
 	fi
     else
-	printf "\e]52;c;%s\a" "$(printf "%s" $CUTBUFFER | base64 --wrap=0)"
+	printf "\e]52;c;%s\a" "$(printf "%s" "$CUTBUFFER" | base64 --wrap=0)"
     fi
 }
